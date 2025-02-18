559646308916fc1a23b7f17a15141c6e6d135128
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-02-18 20:39:22 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,20 @@ const menu = document.getElementById("menu");
 burger.addEventListener("click",()=>{
     console.log("active");
     menu.classList.toggle("active");
-});  
+}); 
 
+let scrollUp = document.querySelector(".up");
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+    let currentScroll = window.scrollY;
+    
+    if (currentScroll > 100) { 
+        scrollUp.classList.add("show"); // Affiche le bouton après 200px de scroll
+    } 
+    
+    if (currentScroll < lastScroll) { 
+        scrollUp.classList.remove("show"); // Cache si on remonte de 50px
+    }
+
+    lastScroll = currentScroll; 
+});
