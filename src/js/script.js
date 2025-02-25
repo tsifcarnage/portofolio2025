486f42f0a918216b51ee1db679d1c531bfc650aa
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-02-25 16:25:11 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,32 @@
 const burger = document.querySelector(".burger");
 const menu = document.getElementById("menu");
-burger.addEventListener("click",()=>{//Pour afficher menu nav en responsive
-    console.log("active");
-    menu.classList.toggle("active");
-}); 
+const nav = document.querySelector("nav");
+nav.style.backgroundColor = "transparent";
+burger.addEventListener("click", () => {
+  nav.style.backgroundColor = "black";
+  menu.style.display = "flex";
+  //Pour afficher menu nav en responsive
+  console.log("active");
+  menu.classList.toggle("active");
+});
 
+function masquerLien() {
+  // Masque le lien
+  nav.style.backgroundColor = "transparent";
+  menu.style.display = "none";
+}
 let scrollUp = document.querySelector(".up");
 let lastScroll = 0;
 window.addEventListener("scroll", () => {
-    let currentScroll = window.scrollY;
-    
-    if (currentScroll > 100) { 
-        scrollUp.classList.add("show"); // Affiche le bouton après 100px de scroll
-    } 
-    
-    if (currentScroll < lastScroll) { 
-        scrollUp.classList.remove("show"); // Cache le bouton si on remonte
-    }
+  let currentScroll = window.scrollY;
 
-    lastScroll = currentScroll; 
+  if (currentScroll > 100) {
+    scrollUp.classList.add("show"); // Affiche le bouton après 100px de scroll
+  }
+
+  if (currentScroll < lastScroll) {
+    scrollUp.classList.remove("show"); // Cache le bouton si on remonte
+  }
+
+  lastScroll = currentScroll;
 });
